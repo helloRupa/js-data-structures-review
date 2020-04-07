e81f455ab98218566216581c027afa8f21215044
Rupa <rupa211984@gmail.com>
2020-04-07 15:33:27 -0700

    Update JS instruction comments

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,3 +1,16 @@
 // Code for converting orders to a data structure goes in here
 // Don't forget to display the result in the result div
 // Hint: You may need JSON.stringify(object)
+
+function convertOrders() {
+
+}
+
+function displayDataStructure(data) {
+  const resultDiv = document.querySelector('.result');
+
+  resultDiv.textContent = data;
+}
+
+// Uncomment the line below when you're ready to test your code
+// displayDataStructure(convertOrders());
