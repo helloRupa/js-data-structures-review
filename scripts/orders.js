aaacb850f72d0190abe0fa790ec64906fbd7013c
Rupa <rupa211984@gmail.com>
2020-04-08 15:03:09 -0700

    Complete orders

--- scripts/orders.js
+++ scripts/orders.js
@@ -5,14 +5,48 @@
 function convertOrders() {
   const orders = document.querySelectorAll('.orders ul');
   const ordersArray = Array.from(orders);
+  const ordersDataStructure = [];
 
-  return ordersArray;
+  for (const ul of ordersArray) {
+    const orderData = {};
+    const orderNumber = ul.querySelector('.order-number span');
+    const total = ul.querySelector('.total span');
+
+    orderData['Order Number'] = orderNumber.textContent;
+    orderData.Total = total.textContent;
+    orderData.Items = convertTableToArray(ul);
+    ordersDataStructure.push(orderData);
+  }
+
+  return ordersDataStructure;
+}
+
+function convertTableToArray(ul) {
+  const table = ul.querySelector('.items table');
+  const headings = table.querySelectorAll('th');
+  const rows = table.querySelectorAll('tr:not(:first-child)');
+  const itemsArray = [];
+
+   for (const row of rows) {
+      const cells = row.querySelectorAll('td');
+      const rowObject = {};
+      
+      for (let hIndex = 0; hIndex < headings.length; hIndex++) {
+        const key = headings[hIndex].textContent;
+
+        rowObject[key] = cells[hIndex].textContent;
+      }
+
+      itemsArray.push(rowObject);
+    }
+
+    return itemsArray;
 }
 
 function displayDataStructure(data) {
   const resultDiv = document.querySelector('.result');
 
-  resultDiv.textContent = data;
+  resultDiv.textContent = JSON.stringify(data);
 }
 
 // Uncomment the line below when you're ready to test your code
